90a2aed4066bc254e92444d2365e2dc38079eab1
sathish <ksathishk9@gmail.com>
2014-10-23 01:23:05 +0100

    Created portal and generated scaffold for status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '085eea0079f1ee5fa0a8e94babed61bf537961d425c059cdd0834594f7f106bafdb6cb5bce4c3cd238240057336fb6fdd0c5ac32519ce377c0ba41dbd84d2c38'
